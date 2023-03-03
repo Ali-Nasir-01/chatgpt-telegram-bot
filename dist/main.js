450b187c9004f85a03a83639190577ed8319469d
nasir <ali.nasir.0104@gmail.com>
2023-03-03 14:06:23 +0330

    Test deployment

---
+++ dist/main.js
@@ -0,0 +1,55 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import TelegramBot from "node-telegram-bot-api";
+import { ChatGPTAPI } from "chatgpt";
+import dotenv from "dotenv";
+dotenv.config();
+const token = process.env.TELEGRAM_TOKEN;
+const apiKey = process.env.CHATGPT_TOKEN;
+const botUsername = process.env.BOT_USERNAME;
+if (!token || !apiKey || !botUsername) {
+    throw "Your .env variables not found!";
+}
+// Initialize Telegram bot
+const bot = new TelegramBot(token, { polling: true });
+// Initialize OpenAI client
+const api = new ChatGPTAPI({
+    apiKey: apiKey,
+});
+const generateResponse = (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!msg)
+        return "";
+    try {
+        const text = msg || "";
+        const response = yield api.sendMessage(text);
+        return response.text;
+    }
+    catch (err) {
+        return "There is no answer";
+    }
+});
+const removeUsername = (msg) => {
+    return msg.replace(`${botUsername} `, "");
+};
+// Handle incoming Telegram messages
+bot.on("message", (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    if (msg.chat.type === "private") {
+        const chatId = msg.chat.id;
+        const response = yield generateResponse(msg.text);
+        bot.sendMessage(chatId, response);
+    }
+}));
+const regex = new RegExp(botUsername, "g");
+bot.onText(regex, (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    const chatId = msg.chat.id;
+    const text = removeUsername(msg.text);
+    const response = yield generateResponse(text);
+    bot.sendMessage(chatId, response);
+}));
