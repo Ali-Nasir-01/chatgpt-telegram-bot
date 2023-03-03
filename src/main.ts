67ac8e8f1e1b02401c069f3282187d480d01c089
nasir <ali.nasir.0104@gmail.com>
2023-03-03 18:03:14 +0330

    Intall node-fetch

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
+import fetch from "node-fetch";
 import { ChatGPTUnofficialProxyAPI } from "chatgpt";
 import dotenv from "dotenv";
 dotenv.config();
